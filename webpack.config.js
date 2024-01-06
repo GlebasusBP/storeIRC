3719252ab7541cdb2a66d071f4f12300c279ab5b
GlebasusBP <bgp13art@Gmail.com>
2024-01-06 16:22:53 +0400

    feat: countres in headers's icons

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: {
+		app: './src/assets/js/index.js'
+	},
+	output: {
+		clean: true,
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	mode: 'development',
+	devServer: {
+		static: './src',
+		compress: true,
+		port: 9000,
+		hot: true,
+		open: {
+			app: {
+				name: 'chrome'
+			}
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(s[ac]ss|css)$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(eot|ttf|woff|woff2|svg|png|jpg|jpeg|gif)$/i,
+				type: "assets/resource",
+			}
+		]
+	},
+	plugins: [
+			new HtmlWebpackPlugin({
+				title: 'My project',
+				template: 'src/index.html',
+			})
+		]
+}
